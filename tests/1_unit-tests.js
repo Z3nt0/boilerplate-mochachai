56eb29badcb3dcdd50bad43db8cf640a123d0ae3
Z3nt0 <marquezanthonyjay@gmail.com>
2025-07-07 22:48:33 +0800

    test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,6 +5,8 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
+      assert.isNull(null,'This is an option error description');
+      assert.isnotNull('1 is not null')
       assert.fail(null, 'This is an optional error description - e.g. null is null');
       assert.fail(1, '1 is not null');
     });
